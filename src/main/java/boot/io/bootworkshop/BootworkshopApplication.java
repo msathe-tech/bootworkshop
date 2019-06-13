21aeb7f1f041a935278e9a2f0b6edf8ac76e2dd8
msathe-tech <msathe@Madhavs-MacBook-Pro.local>
2019-06-13 17:47:23 -0400

    actuator and spring security

--- src/main/java/boot/io/bootworkshop/BootworkshopApplication.java
+++ src/main/java/boot/io/bootworkshop/BootworkshopApplication.java
@@ -1,10 +1,5 @@
 package boot.io.bootworkshop;
 
-import java.util.List;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -12,20 +7,25 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 /* import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 */
 
-import javax.validation.constraints.NotNull;
-import java.util.ArrayList;
-import java.util.Optional;
-import java.util.stream.Stream;
-
 @SpringBootApplication
+@EnableMongoRepositories
 public class BootworkshopApplication {
 
 	@Bean
@@ -75,11 +75,12 @@ class BootWorkshopController {
 	*/
 
 	@GetMapping
-	List<Coffee> getAllCoffees() {
-		List<Coffee> coffees = new ArrayList<Coffee>();
-		repo.findAll().forEach(coffees::add);
-		coffees.forEach(System.out::println);
-		return coffees;
+	Iterable<Coffee> getAllCoffees() {
+		//List<Coffee> coffees = new ArrayList<Coffee>();
+		//repo.findAll().forEach(coffees::add);
+		//coffees.forEach(System.out::println);
+		//return coffees;
+		return repo.findAll();
 	}
 
 	// search for JPA model
@@ -115,8 +116,24 @@ class Coffee {
 		name = "Decaf";
 	}
 
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 	@Id
 	private String id;
+
+	public String getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
 
 	@NotNull
 	private String name;
@@ -127,7 +144,8 @@ class Coffee {
 	}
 }
 
-interface CoffeeRepo extends CrudRepository<Coffee, String> {
+@Repository
+interface CoffeeRepo extends MongoRepository<Coffee, String> {
 	Coffee findByName(String name);
 }
 
