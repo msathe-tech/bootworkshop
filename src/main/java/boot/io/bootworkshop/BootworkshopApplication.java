3de03c2767af4227f160dea1eed0597ce1c9a8cc
msathe-tech <msathe@Madhavs-MacBook-Pro.local>
2019-06-13 10:26:02 -0400

    first commit

---
+++ src/main/java/boot/io/bootworkshop/BootworkshopApplication.java
@@ -0,0 +1,155 @@
+package boot.io.bootworkshop;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.web.bind.annotation.*;
+
+/* import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+*/
+
+import javax.validation.constraints.NotNull;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+@SpringBootApplication
+public class BootworkshopApplication {
+
+	@Bean
+	CommandLineRunner demoData(CoffeeRepo repo) {
+		return args -> {
+			repo.deleteAll();
+
+			Stream.of("Breakfast Blend", "Java", "Hazelnut", "Dark Roast")
+					.map(Coffee::new)
+					.forEach(repo::save);
+
+			repo.findAll().forEach(System.out::println);
+		};
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootworkshopApplication.class, args);
+	}
+
+}
+
+@RestController
+class HelloController {
+	@GetMapping("/hello")
+	String hello() {
+		return "Hello!";
+	}
+}
+
+@RestController
+@RequestMapping("/coffees")
+class BootWorkshopController {
+	@Autowired
+	private CoffeeRepo repo;
+
+	/*
+	public BootWorkshopController(CoffeeRepo repo) {
+		System.out.println("BootWorkshopController constructor");
+		this.repo = repo;
+		repo.deleteAll();
+
+		Stream.of("Breakfast Blend", "Java", "Hazelnut", "Dark Roast")
+				.map(Coffee::new)
+				.forEach(repo::save);
+		repo.findAll().forEach(System.out::println);
+	}
+	*/
+
+	@GetMapping
+	Iterable<Coffee> getAllCoffees() {
+		repo.findAll().forEach(System.out::println);
+		return repo.findAll();
+	}
+
+	// search for JPA model
+	/*
+	@GetMapping("/{id}")
+	Optional<Coffee> getCoffeeById(@PathVariable Long id) {
+		return  repo.findById(id);
+	}
+	*/
+
+	// search for Mongo model
+	@GetMapping("/{id}")
+	Optional<Coffee> getCoffeeById(@PathVariable String id) {
+		System.out.println(repo.findById(id));
+		return repo.findById(id);
+	}
+
+	@GetMapping("/search")
+	Coffee searchForCoffee(@RequestParam String name) {
+		return  repo.findByName(name);
+	}
+}
+
+
+// Model for Mongo
+@Document
+class Coffee {
+	public Coffee(@NotNull String name) {
+		this.name = name;
+	}
+
+	public Coffee() {
+		name = "Decaf";
+	}
+
+	@Id
+	private String id;
+
+	@NotNull
+	private String name;
+
+	@Override
+	public String toString() {
+		return name;
+	}
+}
+
+interface CoffeeRepo extends CrudRepository<Coffee, String> {
+	Coffee findByName(String name);
+}
+
+
+// Model for JPA
+/*
+@Entity
+class Coffee {
+	public Coffee(@NotNull String name) {
+		this.name = name;
+	}
+
+	public Coffee() {
+		name = "Decaf";
+	}
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@NotNull
+	private String name;
+}
+
+interface CoffeeRepo extends CrudRepository<Coffee, Long> {
+	Coffee findByName(String name);
+}
+*/
+
